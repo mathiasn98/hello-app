f6978f0760d02385d0cf18eddb7df62afa40e862
Mathias Novianto <mathiasnn98@gmail.com>
2020-04-27 19:01:16 +0700

    Import html from another file

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,13 +1,24 @@
 import React, { useState } from 'react';
+// import * as Bonjour from './Bonjour.html';
+const html_import = require('./Bonjour.html');
 
 const HelloWorld: React.FC = () => {
   return (
     <>
+        {console.log(html_import)}
+
       <div>Halo dunia</div>
       <div
         dangerouslySetInnerHTML={{
           __html: `<div>Guten Morgen</div>`
         }}></div>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: html_import
+        }}
+      >
+
+      </div>
     </>
   )
 }
